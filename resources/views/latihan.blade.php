1417e570a1e74885225657a442e70f5d0d3c9539
andrapk <andra.rifani@yahoo.com>
2023-05-05 14:26:06 +0700

    update 3

--- resources/views/latihan.blade.php
+++ resources/views/latihan.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.base')
 
 @section('text')
+<img src="/img/cl_primary.png">
 <div class="flex-center position-ref full-height">
 
     <div class="content">
+        
         <div class="title m-b-md">
             Member list
         </div>
-            <div>
-                {{ $member[0]['ign'] }} - {{ $member[0]['skill'] }}
-                {{ $member[1]['ign'] }} - {{ $member[1]['skill'] }}
-                {{ $member[2]['ign'] }} - {{ $member[2]['skill'] }}
-                {{ $member[3]['ign'] }} - {{ $member[3]['skill'] }}
-            </div>        
+             @foreach($member as $members)
+                <div>
+                {{ $members['ign'] }} - {{ $members['skill'] }}
+                </div>
+            @endforeach
     </div>
 </div>
 @endsection
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,61 +6,15 @@
 
         <title>Catalyst Lockdown</title>
 
-        <style>
-            html, body{
-                background-color: #000080;
-                color: #cd2d21;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-
-        </style>
+        <link href="/css/main.css" rel="stylesheet">
     </head>
+
     <body>
         @yield('text')
     </body>
+
+    <footer>
+        Copyright Catalyst Lockdown
+    </footer>
+
 </html>
