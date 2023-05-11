44548fcd9ad87ca0553a1a438faa38646587787c
andrapk <andra.rifani@yahoo.com>
2023-05-12 00:52:36 +0700

    update 4

--- resources/views/latihan.blade.php
+++ resources/views/latihan.blade.php
@@ -1,8 +1,15 @@
 @extends('layouts.base')
 
 @section('text')
-<img src="/img/cl_primary.png">
-<div class="flex-center position-ref full-height">
+
+    
+<div class="row">
+    <div class="col text-center p-3">
+        <img class="logo" src="/img/cl_primary.png">
+    </div>
+</div>
+
+<div class="p-5 full-height">
 
     <div class="content">
         
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,6 +7,8 @@
         <title>Catalyst Lockdown</title>
 
         <link href="/css/main.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     </head>
 
     <body>
