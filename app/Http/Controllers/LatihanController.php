eba45370f4254e95ffec065a54f657d12943d947
andrapk <andra.rifani@yahoo.com>
2023-05-20 01:54:38 +0700

    update 7

--- app/Http/Controllers/LatihanController.php
+++ app/Http/Controllers/LatihanController.php
@@ -8,10 +8,10 @@ class LatihanController extends Controller
 {
     public function index() {
         $member = [
-            ['ign' => 'Vastergotland', 'skill' => 'Sniper'],
-            ['ign' => 'Joker', 'skill' => 'Allround'],
-            ['ign' => 'Mina', 'skill' => 'Strategic'],
-            ['ign' => 'Miyoung', 'skill' => 'Controller']
+            ['ign' => 'Vastergotland', 'skill' => 'Sniper', 'img' => 'KJ.png'],
+            ['ign' => 'Joker', 'skill' => 'Allround', 'img' => 'Jett.png'],
+            ['ign' => 'Mina', 'skill' => 'Strategic', 'img' => 'Phoenix.png'],
+            ['ign' => 'Miyoung', 'skill' => 'Controller', 'img' => 'Viper.png']
         ];
 
         return view('latihan', ['member' => $member]);
--- resources/views/latihan.blade.php
+++ resources/views/latihan.blade.php
@@ -14,58 +14,20 @@
         <div class="title m-b-md">
             Member list
         </div>
-            @foreach($member as $members)
-                <div>
-                    <div class="col-sm-3 p-3 text-black">   
-                        <div class="card" style="width:300px">
-                            <img class="card-img-top" src="/img/KJ.png" alt="Card image" style="width:100%">
-                            <div class="card-body">
-                                <h4 class="card-title">{{ $members['ign'] }}</h4>
-                                <p class="card-text">{{ $members['skill'] }}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endforeach     
-            
+                 
         <div class="container mt-5">
             <div class="row">
-                <div class="col-sm-3 p-3 text-white">   
+                @foreach($member as $members)
+                <div class="col-sm-3 p-3 text-black">   
                     <div class="card" style="width:300px">
-                        <img class="card-img-top" src="/img/KJ.png" alt="Card image" style="width:100%">
+                        <img class="card-img-top" src="/img/{{ $members['img']}}" alt="Card image" style="width:100%">
                         <div class="card-body">
-                            <h4 class="card-title">{{ 'ign '}}</h4>
-                            <p class="card-text">{{ 'skill '}}</p>
+                            <h4 class="card-title">{{ $members['ign'] }}</h4>
+                            <p class="card-text">{{ $members['skill'] }}</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-3 p-3 text-white">   
-                    <div class="card" style="width:300px">
-                        <img class="card-img-top" src="/img/Jett.png" alt="Card image" style="width:100%">
-                        <div class="card-body">
-                            <h4 class="card-title">{{ 'ign '}}</h4>
-                            <p class="card-text">{{ 'skill '}}</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-3 p-3 text-white">   
-                    <div class="card" style="width:300px">
-                        <img class="card-img-top" src="/img/Phoenix.png" alt="Card image" style="width:100%">
-                        <div class="card-body">
-                            <h4 class="card-title">{{ 'ign '}}</h4>
-                            <p class="card-text">{{ 'skill '}}</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-3 p-3 text-white">   
-                    <div class="card" style="width:300px">
-                        <img class="card-img-top" src="/img/Viper.png" alt="Card image" style="width:100%">
-                        <div class="card-body">
-                            <h4 class="card-title">{{ 'ign '}}</h4>
-                            <p class="card-text">{{ 'skill '}}</p>
-                        </div>
-                    </div>
-                </div>        
+                @endforeach    
             </div>
         </div>    
     </div>
