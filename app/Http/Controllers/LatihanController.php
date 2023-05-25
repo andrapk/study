9d116c13d89bfb12107adaf39a104d78b6a5a242
andrapk <andra.rifani@yahoo.com>
2023-05-25 14:54:10 +0700

    update 9 model sudah terbuat tapi tidak terbaca

--- app/Http/Controllers/LatihanController.php
+++ app/Http/Controllers/LatihanController.php
@@ -3,18 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\team;
 
 class LatihanController extends Controller
 {
     public function index() {
-        $member = [
-            ['ign' => 'Vastergotland', 'skill' => 'Sniper', 'img' => 'KJ.png', 'weap' => 'Operator'],
-            ['ign' => 'Joker', 'skill' => 'Allround', 'img' => 'Jett.png', 'weap' => 'Vandal'],
-            ['ign' => 'Mina', 'skill' => 'Strategic', 'img' => 'Phoenix.png', 'weap' => 'Vandal'],
-            ['ign' => 'Miyoung', 'skill' => 'Controller', 'img' => 'Viper.png', 'weap' => 'Phantom']
-        ];
 
-        return view('latihan', ['member' => $member]);
+        $member = team::all();
+
+        return view('latihan', [
+            'member' => $member]);
     }
 
     public function show($id) {
